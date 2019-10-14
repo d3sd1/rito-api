cbbba43bde09ba50f15483da49ca9059002672d8
Andrei <ag00631981@techmahindra.com>
2019-10-14 19:02:40 +0200

    fix for champ masteries

--- src/main/java/com/onlol/fetcher/api/model/SummonerChampionMastery.java
+++ src/main/java/com/onlol/fetcher/api/model/SummonerChampionMastery.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 public class SummonerChampionMastery {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, unique = true)
     private Long id;
 
