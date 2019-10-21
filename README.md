4494bfcc7ac8ed96207ab372cdcb89faf34152fc
Andrei <ag00631981@techmahindra.com>
2019-10-21 18:29:14 +0200

    updated todos added ideas to redme

---
+++ README.md
@@ -0,0 +1,10 @@
+#IDEAS generales
+
+- Añadir a web lol: cambios respecto al último parche del campeón en stats de forma automática en el visor del campeón
+- Notificador por telegram email etc de los parches nuevos, y los cambios del parche (incluido navegador)
+- notificador de
+- sincronizador de movil con web, para que puedan usar app web/móvil al mismo tiempo y ver los mismos datos. Manejan la app del móvil y pueden ver datos en pantalla
+- que avise de los errores por email en cualquier excepción
+- streams de usuarios en el perfil
+-
+ 
--- src/main/java/com/onlol/fetcher/api/connector/DdragonConnector.java
+++ src/main/java/com/onlol/fetcher/api/connector/DdragonConnector.java
@@ -19,18 +19,8 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.*;
 
-//TODO: añadir logging en consola / mail auto / db para ver que pasa
 //TODO: que se mande mail cuando todo pete
-//TODO: ideas
-/*
-- Añadir a web lol: cambios respecto al último parche del campeón en stats de forma automática en el visor del campeón
-- Notificador por telegram email etc de los parches nuevos, y los cambios del parche (incluido navegador)
-- notificador de
-- sincronizador de movil con web, para que puedan usar app web/móvil al mismo tiempo y ver los mismos datos. Manejan la app del móvil y pueden ver datos en pantalla
-- que avise de los errores por email en cualquier excepción
-- streams de usuarios en el perfil
--
- */
+/
 @Service
 public class DdragonConnector {
 
