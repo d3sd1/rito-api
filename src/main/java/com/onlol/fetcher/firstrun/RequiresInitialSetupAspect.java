b1a1382fba9893a3fd76cbdb1d497967f8c5074a
Andrei <ag00631981@techmahindra.com>
2019-11-08 13:36:30 +0100

    upgraded logger only shown 1 time for initial setup

--- src/main/java/com/onlol/fetcher/firstrun/RequiresInitialSetupAspect.java
+++ src/main/java/com/onlol/fetcher/firstrun/RequiresInitialSetupAspect.java
@@ -17,12 +17,16 @@ public class RequiresInitialSetupAspect {
     @Autowired
     private RunLogRepository runLogRepository;
 
+    private boolean messageShown = true;
+
     @Around("@annotation(RequiresInitialSetup)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        if(this.runLogRepository.findAll().isEmpty()) {
+        if (this.runLogRepository.findAll().isEmpty() && !messageShown) {
             this.logger.debug("Prevented task for being executed before the initial setup is finished.");
+            messageShown = true;
             return null;
         }
+        messageShown = false;
         return joinPoint.proceed();
     }
 }
