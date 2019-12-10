0942a7cc87289cf447c5201ddaf6eeadc5520637
Andrei <ag00631981@techmahindra.com>
2019-12-10 10:18:54 +0100

    added api key migration added config for reloading database on every run

---
+++ src/main/resources/db/all/migration/V1.0__api_base_key.sql
@@ -0,0 +1,4 @@
+INSERT INTO api_key (api_key, banned, valid, last_timestamp_used, retry_after, invalid_calls)
+VALUES ('RGAPI-1fee5177-2856-46d9-9519-7716861487fe', false, true, 0, 0, 0),
+       ('RGAPI-d65da2c7-5339-421e-822c-5b4e36b1dc7f', false, true, 0, 0, 0),
+       ('RGAPI-bf12658b-d9c8-45d1-8016-caca0b5d0d62', false, true, 0, 0, 0);
