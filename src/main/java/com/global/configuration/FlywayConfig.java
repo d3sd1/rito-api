5b1169fc2e8a9da29e15b053b2c7c2241fa31957
Andrei García <andreigarciacuadra@gmail.com>
2019-12-14 18:24:55 +0100

    fix for schemas error                   fix for migration error                   configured environments                   now DEV always flushes database and clears it on every execution                   added idea hotswap
                      RIOT-106 #close #comment corrected indent issue

--- src/main/java/com/global/configuration/FlywayConfig.java
+++ src/main/java/com/global/configuration/FlywayConfig.java
@@ -2,17 +2,23 @@ package com.global.configuration;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
-public class MigrationConfiguration implements ApplicationListener<ApplicationStartedEvent> {
+public class FlywayConfig implements ApplicationListener<ApplicationStartedEvent> {
 
     @Autowired
-    Flyway flyway;
+    private Flyway flyway;
+
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     /**
      * Override default flyway initializer to do nothing
@@ -23,7 +29,23 @@ public class MigrationConfiguration implements ApplicationListener<ApplicationSt
         });
     }
 
+    /**
+     * Create a second flyway initializer to run after jpa has created the schema
+     */
+    @DependsOn("entityManagerFactory")
+    FlywayMigrationInitializer delayedFlywayInitializer(Flyway flyway) {
+        return new FlywayMigrationInitializer(flyway, null);
+    }
+
+    void generateSchemas() {
+        //this.entityManager.createQuery("CREATE SCHEMA IF NOT EXISTS global;").executeUpdate();
+    }
+
+
     public void onApplicationEvent(ApplicationStartedEvent contextRefreshedEvent) {
+        if (activeProfile.equalsIgnoreCase("dev")) {
+
+        }
         this.flyway.migrate();
     }
 
