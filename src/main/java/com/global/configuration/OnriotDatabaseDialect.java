4c728d7a453919f875ac5c583ce9b8ba9e3765db
Andrei García <andreigarciacuadra@gmail.com>
2019-12-16 11:49:04 +0100

    RIOT-113 #close #comment Mejorada dependencia, agregado nuevo dialecto de postgre para que en el "create-drop" se elimine "if exists", y de ese modo no salgan esos absurdos y burdos errores por consola.

---
+++ src/main/java/com/global/configuration/OnriotDatabaseDialect.java
@@ -0,0 +1,19 @@
+package com.global.configuration;
+
+import org.hibernate.dialect.PostgreSQL10Dialect;
+
+public class OnriotDatabaseDialect extends PostgreSQL10Dialect {
+
+    @Override
+    public String getDropSequenceString(String sequenceName) {
+        // Adding the "if exists" clause to avoid warnings
+        return "drop sequence if exists " + sequenceName;
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        // We don't need to drop constraints before dropping tables, that just leads to error
+        // messages about missing tables when we don't have a schema in the database
+        return false;
+    }
+}
