c8786f8f3bd89f1962d1a4dbbef9bd502965f653
Andrei García <andreigarciacuadra@gmail.com>
2019-12-16 10:29:58 +0100

    RIOT-102 #inprogress #comment Added required iinitial games migration, and api endpoint model

--- src/main/java/com/global/model/ApiEndpoint.java
+++ src/main/java/com/global/model/ApiEndpoint.java
@@ -1,6 +1,7 @@
 package com.global.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Entity
@@ -11,15 +12,16 @@ public class ApiEndpoint {
     @Column(nullable = false, unique = true)
     private long id;
 
-/*
     @Column(nullable = false, unique = true)
     private String endpoint;
+
     @OneToMany
-    private List<Platform> platforms;
+    private
+    List<Platform> platforms;
 
     @Column(nullable = false, unique = false)
     private boolean disabled;
-/*
+
     enum ApiMethod
     {
         GET, POST, PUT, DELETE
@@ -32,6 +34,6 @@ public class ApiEndpoint {
     private boolean requiresApiKey;
 
     @OneToOne
-    private RiotGame riotGame;*/
+    private RiotGame riotGame;
 
 }
---
+++ src/main/resources/db/migration/common/V0.0.3__riot_initial_games.sql
@@ -0,0 +1,6 @@
+SET search_path TO common,logging,onriot,onlol,ontft,onlor,onlwr;
+INSERT INTO riotgame (gamename, description)
+VALUES ('LoL', 'League of Legends'),
+       ('TFT', 'TeamFight Tactics'),
+       ('LoR', 'Legends of Runaterra'),
+       ('LWR', 'Legends of Legends: WildRift');
