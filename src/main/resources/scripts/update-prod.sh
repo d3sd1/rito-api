10a840d8461d9eb446f13f3992dd28de8f5e0276
Andrei García <andreigarciacuadra@gmail.com>
2019-12-19 19:37:33 +0100

    RIOT-109 #in-progress #comment Deleted server scripts and updated pipeline to get it working without cron (hot-swap live version).

--- src/main/resources/scripts/update-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -9 java
-nohup java -Dspring.profiles.active=prod -jar /var/www/*.jar &> /dev/null &
-disown
-rm -- "$0"
--- src/main/resources/scripts/update-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -9 java
-nohup java -Dspring.profiles.active=test -jar /var/www/*.jar &> /dev/null &
-disown
-rm -- "$0"
