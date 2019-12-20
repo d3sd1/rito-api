beb730cd7427434676fee9f1b9d66e5814509323
Andrei García <andreigarciacuadra@gmail.com>
2019-12-20 01:25:41 +0100

    RIOT-109 #in-progress #comment Fix for env path.

--- src/main/resources/scripts/run-scraper.sh
+++ src/main/resources/scripts/run-scraper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup java -Dspring.profiles.active=$(cat 'env') -jar /var/www/*.jar &> /dev/null &
+nohup java -Dspring.profiles.active=$(cat 'env.conf') -jar /var/www/*.jar &> /dev/null &
 disown
 rm -- "$0"
