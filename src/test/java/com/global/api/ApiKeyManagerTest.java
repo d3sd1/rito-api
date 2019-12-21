828677cea10cf27115113eae511ad6a44e2d22ea
Andrei García <andreigarciacuadra@gmail.com>
2019-12-21 14:11:28 +0100

    Added test

---
+++ src/test/java/com/global/api/ApiKeyManagerTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019.
+ * d3sd1.
+ * All right reserved.
+ * Do not re-distribute this file nor project without permission.
+ */
+
+package com.global.api;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiKeyManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void regenerateApiKeys() {
+    }
+
+    @Test
+    void getKey() {
+
+    }
+
+    @Test
+    void banKey() {
+    }
+}
