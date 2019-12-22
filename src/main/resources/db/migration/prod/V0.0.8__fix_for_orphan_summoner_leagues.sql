0747e15fdd7c8d352bd5951a84c9907accb7598f
Andrei García <andreigarciacuadra@gmail.com>
2019-12-22 06:56:49 +0100

    hotpatch for preventing null summoner entities on leagues -> added query

---
+++ src/main/resources/db/migration/prod/V0.0.8__fix_for_orphan_summoner_leagues.sql
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019.
+ * d3sd1.
+ * All right reserved.
+ * Do not re-distribute this file nor project without permission.
+ */
+
+SET search_path TO common,logging,onriot,onlol,ontft,onlor,onlwr;
+DELETE
+FROM onlol.leagueentry
+WHERE summoner_id = null;
